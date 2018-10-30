8dd2ec3e9b28f1130cf721ba92e669a10b81d4bc
Alinx <zqrlsoer@outlook.com>
2018-10-30 14:09:19 +0800

    Site updated: 2018-10-30 14:09:17

--- js/particles.other.js
+++ js/particles.other.js
@@ -10,7 +10,7 @@ $(function () {
                 }
             },
             "color": {
-                "value": "#399c9c"
+                "value": "#302F31"
             },
             "shape": {
                 "type": "circle",
